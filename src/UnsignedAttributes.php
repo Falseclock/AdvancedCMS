a73eaada7c395660d8516d025d658c79404b6faa
Nurlan Mukhanov <102@mp.kz>
2020-12-07 17:33:38 +0600

    fix return until full pull request

--- src/UnsignedAttributes.php
+++ src/UnsignedAttributes.php
@@ -13,7 +13,6 @@ namespace Falseclock\AdvancedCMS;
 use Adapik\CMS\BasicOCSPResponse;
 use Adapik\CMS\CertificateList;
 use Adapik\CMS\Interfaces\CMSInterface;
-use Adapik\CMS\TimeStampToken;
 use Adapik\CMS\UnsignedAttribute;
 use FG\ASN1\Exception\Exception;
 use FG\ASN1\Exception\ParserException;
@@ -89,7 +88,7 @@ class UnsignedAttributes extends \Adapik\CMS\UnsignedAttributes
     /**
      * @return TimeStampToken|CMSInterface|null
      */
-    public function getTimeStampToken(): ?TimeStampToken
+    public function getTimeStampToken()
     {
         return $this->getAttributeAsInstance(TimeStampToken::class);
     }
